f4fe965666713f37bb7847ec00a8171b8221d1de
depikaselvaraj <85164601+depikaselvaraj@users.noreply.github.com>
2021-06-22 16:06:18 +0530

    JpaCrudRepositoryexample
    JpaCrud

---
+++ demo/target/classes/data.sql
@@ -0,0 +1,2 @@
+insert into EMPLOYEES(id,firstname,lastname,email)values(1,'deepika','selvaraj','deep@1808')
+insert into EMPLOYEES(id,firstname,lastname,email)values(2,'mohana','loganathan','mona@1808')
